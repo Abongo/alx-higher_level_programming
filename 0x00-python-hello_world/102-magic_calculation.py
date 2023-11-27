3dd2a8c40fad761161b0a3007e88146378cb1ad7
abongo <abongoje@gmail.com>
2023-11-27 12:51:32 -0800

    101-compile

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    magic_number = 98  
-    result = a ** b  
-    result += magic_number  
+    magic_number = 98
+    result = a ** b
+    result += magic_number
     return result
