ac9118649efd19c84b7b9cc9e2aa88e8e28f1da7
abongo <abongoje@gmail.com>
2024-01-10 13:04:28 -0800

    updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-
 """Integer addition"""
 
-
 def add_integer(a, b=98):
-    """
-    Function adds two integers
-    """
-    if isinstance(a, (int, float)) is False:
-        raise TypeError('a must be an integer')
-    if isinstance(b, (int, float)) is False:
-            raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
